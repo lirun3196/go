5dd8ac6a96f358dbff7949d3bd66c821aaf1c4de
lirun3196 <lirun3196@163.com>
2014-11-09 01:03:10 +0800

    change index.html again

--- js/findPass.js
+++ js/findPass.js
@@ -2,5 +2,8 @@ $(".phone , .email").on("click",function(){
   $(".inputEmail , .inputPhone").hide();
   $(this).next("span").show();
 });
-
-
+$(".btn").on("click",function(){
+  $(".container-fluid").load("findPass2.html #password",function(){
+    $(this).fadeOut(300).fadeIn(300);
+});
+});
