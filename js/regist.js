7a54eb927e6cee179815b2057d8b450a809e5507
lirun3196 <lirun3196@163.com>
2014-11-08 18:57:24 +0800

    change a lot

--- js/regist.js
+++ js/regist.js
@@ -6,7 +6,7 @@ check.on("click",function(){
 //focus+翻牌效果
 var i = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 $.each(i,function(i){
-  $(".input"+i).on("focus",function(){
-    $(".over"+ i).addClass("over");
+  $(".input"+i).on("change",function(){
+    $(".over"+ i).addClass("over").children("img").show();
   });
 });
