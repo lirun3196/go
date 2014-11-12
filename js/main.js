ee8afca7b9cdf3524a00f92510fc48767d8d205f
lirun3196 <lirun3196@163.com>
2014-11-13 00:03:00 +0800

    change crousel

--- js/main.js
+++ js/main.js
@@ -61,5 +61,54 @@
 //      }
 //    });
 }(jQuery);
+/*var main = {
+  onReady:function(){
+    var h = $(".right").height();
+    parseInt(h) > 1580 ? $(".left , .menu").height(h) : $(".left , .menu").height(1580);
+  },
+  secHover:function(){
+    // 内容板块hover效果
+    var sec=$(".section-item");
+    sec.each(function(){
+      $(this).hover(function(){
+        $(this).children('.action').toggleClass('actioned');
+      });
+    });
+  },
+  detectW:function(){
+    // 探测屏幕宽度，以隐藏/显示.left类
+    var screenW = $(window).width();
+    var contentW = 1530;
+    if(contentW > screenW){
+      $(".left").width(0);
+    }else{
+      $(".wraper,.footer").css("min-width","1530px");
+    };
+  },
+  nextG:function(){
+    // "换一组"
+    $(".change").on("click",function(){
+      $(this).parent().prevAll(".section-items").toggleClass("show-item");
+    });
+  },
+  navCenter:function(){
+    // 中部导航hover效果
+    var i = $(".nav-center a");
+    var c = $(".center-active");
+    c.prev("img").addClass("show-img");
+    i.hover(function(event){
+      event.stopPropagation();
+      c.prev("img").toggleClass("show-img");
+      $(this).prev("img").toggleClass("show-img");
+    });
+  },
+  myBox:function(){
+    //checkbox点击效果
+    var check = $(".checkIcon");
+    check.on("click",function(){
+      $(this).toggleClass("checkIconed");
+    });
+  }
+};*/
 
 
