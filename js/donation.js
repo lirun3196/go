5b2f1eaf42b6bbb68a61d3fb8ee3a5827349589a
lirun3196 <lirun3196@163.com>
2014-11-12 17:50:59 +0800

    add esoDetail.html

--- js/donation.js
+++ js/donation.js
@@ -1,3 +1,12 @@
 $('a[href="#ranklist"],a[href="#myDonate"]').on("click",function(){
   $(".section.donate-record").toggleClass("hiddenRecord");
 });
+ //捐赠轮播
+var donateC = new Swiper('.donate-container',{
+  pagination: '.pagination',
+  paginationClickable: true,
+  loop: true,
+  autoplay: true,
+  speed: 5000,
+  calculateHeight: true
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
- $(function(){
++function ($){
+   // 探测.content高度，以决定.left,.menu的高度
+    var h = $(".right").height();
+    parseInt(h) > 1580 ? $(".left , .menu").height(h) : $(".left , .menu").height(1580);
     // 内容板块hover效果
     var sec=$(".section-item");
     sec.each(function(){
@@ -30,33 +33,33 @@
       $(this).prev("img").toggleClass("show-img");
     });
     //广告
-    var mySwiper = new Swiper('.swiper-container',{
-      //pagination: '.pagination',
-      //paginationClickable: true,
-      loop: true,
-      autoplay: true,
-      speed: 3000,
-      calculateHeight: true
-    });
+//    var mySwiper = new Swiper('.swiper-container',{
+//      //pagination: '.pagination',
+//      //paginationClickable: true,
+//      loop: true,
+//      autoplay: true,
+//      speed: 3000,
+//      calculateHeight: true
+//    });
     //checkbox点击效果
     var check = $(".checkIcon");
     check.on("click",function(){
       $(this).toggleClass("checkIconed");
     });
     //点击→轮播
-    $(".rightArrow, .leftArrow").on("click",function(){
-      var crousel = $(".crouselContent");
-      var off = crousel.css("left");
-      if(parseInt(off) === 0){
-        crousel.animate({
-          left:"-695px"
-        },300,"linear");
-      }else{
-        crousel.animate({
-          left:"0px"
-        },300,"linear");
-      }
-    });
-});
+//    $(".rightArrow, .leftArrow").on("click",function(){
+//      var crousel = $(".crouselContent");
+//      var off = crousel.css("left");
+//      if(parseInt(off) === 0){
+//        crousel.animate({
+//          left:"-695px"
+//        },300,"linear");
+//      }else{
+//        crousel.animate({
+//          left:"0px"
+//        },300,"linear");
+//      }
+//    });
+}(jQuery);
 
 
