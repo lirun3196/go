7f106e1e38fe551969a2ce4db4747e26ec61c377
lirun3196 <lirun3196@163.com>
2014-11-14 00:04:19 +0800

    change donation.html

--- js/donation.js
+++ js/donation.js
@@ -1,12 +1,7 @@
-$('a[href="#ranklist"],a[href="#myDonate"]').on("click",function(){
-  $(".section.donate-record").toggleClass("hiddenRecord");
-});
- //捐赠轮播
-var donateC = new Swiper('.donate-container',{
-  pagination: '.pagination',
-  paginationClickable: true,
-  loop: true,
-  autoplay: true,
-  speed: 5000,
-  calculateHeight: true
-});
+var c = {
+  scrollD:function(){
+   $(".dCrousel").hover(function(){
+     $(".caret-down");
+   }); 
+  }
+};
