2714f756f11a471c4c671ee13ff71224cfcc0b3d
lirun3196 <lirun3196@163.com>
2014-11-22 18:01:06 +0800

    version 1.1

--- js/setting.js
+++
@@ -1,9 +0,0 @@
-var arr = ["setCode","setEmail","setPhone"];
-$.each(arr,function(i,val){
-  $("#" + val).on("click",function(e){
-    e.preventDefault();
-    $("html").load(val+".html",function(){
-//    $(this).fadeOut(300).fadeIn(300);
-    });
-  });
-});
